c88e7dd5eb82dde89b5ecfa9d84a5b8e3ee95f1d
Skysoul <rafael@skysoul.com.au>
2016-04-15 11:02:44 +0800

    Added hasRoles method

--- app/Models/Traits/RolesAndPermissionTrait.php
+++ app/Models/Traits/RolesAndPermissionTrait.php
@@ -3,8 +3,12 @@
 namespace DreamsArk\Models\Traits;
 
 use DreamsArk\Models\User\Role;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
+/**
+ * Class RolesAndPermissionTrait
+ *
+ * @package DreamsArk\Models\Traits
+ */
 trait RolesAndPermissionTrait
 {
     /**
@@ -16,9 +20,29 @@ trait RolesAndPermissionTrait
     }
 
     /**
-     * Check if this model has a given role
+     * Check if user has multiple roles
      *
-     * @param string $name
+     * @param array $roles
+     * @return bool
+     */
+    public function hasRoles(array $roles)
+    {
+
+        $result = [];
+
+        foreach ($roles as $role)
+            array_push($result, $this->hasRole($role));
+
+        return count($roles) === count(array_filter($result));
+
+    }
+
+    /**
+     * Check if this model has a given role
+     * Usage hasRole(user, admin) will be read like has role user or admin?
+
+     *
+*@param string $name
      * @return bool
      */
     public function hasRole($name)
@@ -29,27 +53,18 @@ trait RolesAndPermissionTrait
          */
         if (func_num_args() > 1) {
 
-            foreach (func_get_args() as $role) {
-
-                if ($this->hasRole($role)) {
-                    return true;
-                };
-
-            }
+            foreach (func_get_args() as $role)
+                if ($this->hasRole($role)) return true;
 
             return false;
 
         }
 
-        foreach ($this->roles as $role) {
-            if ($role->name == $name) {
-                return true;
-            }
-        }
+        foreach ($this->roles as $role)
+            if ($role->name == strtolower($name)) return true;
 
         return false;
 
     }
-
 
 }
