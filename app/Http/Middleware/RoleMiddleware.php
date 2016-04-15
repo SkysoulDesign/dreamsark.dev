233d78d526a118ca3904b330a890296361e3a90b
Skysoul <rafael@skysoul.com.au>
2016-04-15 13:13:34 +0800

    Added or case to hasRoles Functionality

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -4,26 +4,57 @@ namespace DreamsArk\Http\Middleware;
 
 use Closure;
 
+/**
+ * Class RoleMiddleware
+ *
+ * @package DreamsArk\Http\Middleware
+ */
 class RoleMiddleware
 {
     /**
      * Handle an incoming request.
+
      *
-     * @param  \Illuminate\Http\Request $request
+*@param  \Illuminate\Http\Request $request
      * @param  \Closure $next
-     * @param $role
+     * @param $roles
      * @return mixed
      */
-    public function handle($request, Closure $next, $role)
+    public function handle($request, Closure $next, $roles)
     {
-        $message = 'You have no permissions to open this URL';
-        if (substr($role, 0, 1) == '!') {
-            if ($request->user()->is(str_replace('!', '', $role))) {
-                return redirect()->back()->withErrors($message);
-            }
-        } else if (!$request->user()->is($role)) {
-            return redirect()->back()->withErrors($message);
+
+        /**
+         * if Not logged in Redirect to Login
+         */
+        if (!$user = $request->user()) {
+            return redirect()->guest(route('login'));
         }
+
+        /**
+         * Parse Arguments
+         */
+        $arguments = $this->parseArgs(array_slice(func_get_args(), 2));
+
+        if (!call_user_func_array([$user, "hasRoles"], $arguments)) {
+            return response('Unauthorized, user has no assigned roles', 401);
+        }
+
         return $next($request);
+
     }
+
+    /**
+     * Parse pipes on arguments
+     *
+     * @param $args
+     * @return mixed
+     */
+    public function parseArgs($args)
+    {
+        return array_map(function ($arg) {
+            return str_contains($arg, '|') ? explode('|', $arg) : $arg;
+        }, $args);
+
+    }
+
 }
--- app/Models/Traits/RolesAndPermissionTrait.php
+++ app/Models/Traits/RolesAndPermissionTrait.php
@@ -20,24 +20,6 @@ trait RolesAndPermissionTrait
     }
 
     /**
-     * Check if user has multiple roles
-     *
-     * @param array $roles
-     * @return bool
-     */
-    public function hasRoles(array $roles)
-    {
-
-        $result = [];
-
-        foreach ($roles as $role)
-            array_push($result, $this->hasRole($role));
-
-        return count($roles) === count(array_filter($result));
-
-    }
-
-    /**
      * Check if this model has a given role
      * Usage hasRole(user, admin) will be read like has role user or admin?
      *
@@ -63,6 +45,27 @@ trait RolesAndPermissionTrait
             if ($role->name == strtolower($name)) return true;
 
         return false;
+
+    }
+
+
+    /**
+     * Check if user has multiple roles
+     *
+     * @param array $roles
+     * @return bool
+     */
+    public function hasRoles($roles)
+    {
+
+        $result = [];
+
+        foreach (func_get_args() as $role) {
+            $function = "call_user_func" . (is_array($role) ? '_array' : null);
+            array_push($result, $function([$this, 'hasRole'], $role));
+        }
+
+        return count(func_get_args()) === count(array_filter($result));
 
     }
 
