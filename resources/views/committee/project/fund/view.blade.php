83c2a5d8d61ea866315a752eaa189a4fd375134e
Vivekanandan Krishnan <vivek@skysoul.com.au>
2016-05-06 15:56:32 +0800

    Committee Section: 1. New page for Fund stage Listing 2. In Fund View page, displayed Project plan, Enroller & Backer list

--- resources/views/committee/project/fund/view.blade.php
+++ resources/views/committee/project/fund/view.blade.php
@@ -2,28 +2,47 @@
 
 @section('content')
     <div class="column">
-            <div class="ui menu">
-                <div class="header item">
-                    @lang('project.view-fund'):&nbsp;{{ $fund->project->name }}
+        <div class="ui menu">
+            <div class="header item">
+                @lang('project.view-fund'):&nbsp;{{ $fund->project->name }}
+            </div>
+            <div class="right menu">
+                @include('admin.partials.project.view-project-button', ['project_id' => $fund->project_id, 'class' => ' item'])
+            </div>
+        </div>
+        <div class="ui segments">
+            <div class="ui tabular top attached three item menu">
+                <div class="active item" data-tab="project">
+                    <i class="icon sitemap"></i>
+                    @lang('project.plan-data')
                 </div>
-                <div class="right menu">
-                    @include('admin.partials.project.view-project-button', ['project_id' => $fund->project_id, 'class' => ' item'])
+                <div class="item" data-tab="enroll">
+                    <i class="icon users"></i>
+                    @lang('project.enroll-list')
+                </div>
+                <div class="item" data-tab="backer">
+                    <i class="icon money"></i>
+                    @lang('project.backers')
                 </div>
             </div>
-            <div class="ui segments">
-                <div class="ui segment project-plan">
-                    @include('committee.project.fund.partials.project-plan', ['enrollable' => $fund->project->enrollable, 'expensable' => $fund->project->expensable])
-                </div>
-                <div class="ui segment enroll-list">
-                    @include('committee.project.fund.partials.enroll-list', ['enrollable' => $fund->project->enrollable])
-                </div>
-                <div class="ui segment backer-list">
-                    @include('committee.project.fund.partials.backer-list', ['backers' => $fund->project->backers])
-                </div>
+            <div class="ui active bottom attached tab segment project-plan" data-tab="project">
+                @include('committee.project.fund.partials.project-plan', ['enrollable' => $fund->project->enrollable, 'expensable' => $fund->project->expensable])
             </div>
+            <div class="ui tab bottom attached segment enroll-list" data-tab="enroll">
+                @include('committee.project.fund.partials.enroll-list', ['enrollable' => $fund->project->enrollable])
+            </div>
+            <div class="ui tab bottom attached segment backer-list" data-tab="backer">
+                @include('committee.project.fund.partials.backer-list', ['backers' => $fund->project->backers])
+            </div>
+        </div>
     </div>
 @endsection
 
 @section('pos-scripts')
     @include('partials.embed-show-project-script')
+    <style>
+        .tabular.menu i.icon {
+            font-size: 2em;
+        }
+    </style>
 @endsection
