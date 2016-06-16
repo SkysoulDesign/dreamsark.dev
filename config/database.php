175f86b1667e2169fa91918d143dddde31e7070f
Skysoul <rafael@skysoul.com.au>
2016-06-16 10:38:51 +0800

    update

--- config/database.php
+++ config/database.php
@@ -140,7 +140,7 @@ return [
         'cluster' => false,
 
         'default' => [
-            'host'     => '127.0.0.1',
+            'host'     => env('BROADCAST_HOST', '127.0.0.1'),
             'port'     => 6379,
             'database' => 0,
         ],
