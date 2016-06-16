51a3f84d425eaaed38ef614f56fbaa1a8b499474
Skysoul <rafael@skysoul.com.au>
2016-06-16 18:35:47 +0800

    update

--- resources/assets/typescript/App.ts
+++ resources/assets/typescript/App.ts
@@ -18,15 +18,61 @@ export class App {
 }
 
 let app = new App();
-    app.ready().then(application => {
+app.ready().then(application => {
 
-        // new Vue({
-        //     el:'#app-root',
-        //     template: require('./templates/segment.html'),
-        //     data: {
-        //         test: 'as world'
-        //     },
-        //
-        // });
+    var rippleButton = Vue.component('ripple-button', {
+        template: require('./templates/ripple-button.html'),
+        props: ['type'],
+        methods: {
+            submit(e:MouseEvent) {
+                e.preventDefault();
+                console.dir();
+            }
+        },
+        data() {
+            return {
+                test: 'hello world'
+            }
+        },
+        ready(){
 
+        }
     });
+
+    var input = Vue.component('ark-input', {
+        template: require('./templates/form/input.html'),
+        props: ['name', 'placeholder', 'type'],
+    });
+
+    Vue.component('ark-form', {
+        template: require('./templates/form/form.html'),
+        props: ['action', 'method'],
+        data: () => {
+            return {
+                model: 'im model'
+            }
+        },
+        components: [
+            input
+        ]
+    });
+
+    // var child = Vue.component('ark-input', {
+    //     template: '<div class="test">testchild</div>'
+    // });
+    //
+    // var parent = Vue.component('ark-form', {
+    //     template: '<div class="test"><slot></slot></div>',
+    //     components: {
+    //         child: child
+    //     }
+    // });
+
+    new Vue({
+        el: '#app-root',
+        ready: function () {
+            console.log('hello world')
+        }
+    });
+
+});
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,290 +3,94 @@
 @section('content')
 
     <div class="centered">
+
         <div class="segments">
 
             <div class="segment --12-columns-mobile --3-columns-desktop --8-offset-desktop">
 
-                <form action="{{ route('login.store') }}" method="POST">
+                <div class="segment__content">
 
-                    <div class="form__field">
-                        <input name="login" type="text" placeholder="username or email">
-                    </div>
+                    <header class="header --classic">
+                        Login Form
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    </header>
 
-                    <div class="form__field">
-                        <input name="password" type="password" placeholder="password">
-                    </div>
-
-                    <div class="form__message --error">
-                        <div class="form__message__header">
-                            Oops... something went wrong...
-                        </div>
-                        <ul class="form__message__items">
-                            <li>The login field is required.</li>
-                        </ul>
-                    </div>
-
-                    <div class="form__field">
-                        <button id="js-ripple-btn" class="button --ripple">
-
+                    <ark-form action="{{ route('login.store') }}" method="post">
+                        <ark-input name="login" placeholder="username or email"></ark-input>
+                        <ark-input name="password" type="password"></ark-input>
+                        <ripple-button type="submit">
                             Login
+                        </ripple-button>
+                    </ark-form>
 
-                            <svg class="ripple-obj" id="js-ripple">
-                                <use width="4" height="4" xlink:href="#dreamsark-polygon" class="js-ripple"></use>
-                            </svg>
+                    {{--<form action="{{ route('login.store') }}" method="POST">--}}
 
-                        </button>
-                        <div style="height: 0; width: 0; position: absolute; visibility: hidden;" aria-hidden="true">
-                            <svg version="1.1" xmlns="http://www.w3.org/2000/svg"
-                                 xmlns:xlink="http://www.w3.org/1999/xlink"
-                                 focusable="false">
-                                <symbol id="dreamsark-polygon" viewBox="0 0 100 100">
-                                    <g>
-                                        <polygon points="5.6,77.4 0,29 39.1,0 83.8,19.3 89.4,67.7 50.3,96.7"></polygon>
-                                        <polygon fill="rgba(255,255,255,0.35)" transform="scale(0.5), translate(50, 50)"
-                                                 points="5.6,77.4 0,29 39.1,0 83.8,19.3 89.4,67.7 50.3,96.7"></polygon>
-                                        <polygon fill="rgba(255,255,255,0.25)"
-                                                 transform="scale(0.25), translate(145, 145)"
-                                                 points="5.6,77.4 0,29 39.1,0 83.8,19.3 89.4,67.7 50.3,96.7"></polygon>
-                                    </g>
-                                </symbol>
-                            </svg>
-                        </div>
+                        {{--<div class="form__field">--}}
+                            {{--<input name="login" type="text" placeholder="username or email">--}}
+                        {{--</div>--}}
+
+                        {{--<div class="form__field">--}}
+                            {{--<input name="password" type="password" placeholder="password">--}}
+                        {{--</div>--}}
+
+                        {{--<div class="form__message --error">--}}
+                            {{--<div class="form__message__header">--}}
+                                {{--Oops... something went wrong...--}}
+                            {{--</div>--}}
+                            {{--<ul class="form__message__items">--}}
+                                {{--<li>The login field is required.</li>--}}
+                            {{--</ul>--}}
+                        {{--</div>--}}
+
+                        {{--<div class="form__field">--}}
+                            {{--<button id="js-ripple-btn" class="button --ripple">--}}
+
+                                {{--Login--}}
+
+                                {{--<svg class="ripple-obj" id="js-ripple">--}}
+                                    {{--<use width="4" height="4" xlink:href="#dreamsark-polygon" class="js-ripple"></use>--}}
+                                {{--</svg>--}}
+
+                            {{--</button>--}}
+                            {{--<div style="height: 0; width: 0; position: absolute; visibility: hidden;"--}}
+                                 {{--aria-hidden="true">--}}
+                                {{--<svg version="1.1" xmlns="http://www.w3.org/2000/svg"--}}
+                                     {{--xmlns:xlink="http://www.w3.org/1999/xlink"--}}
+                                     {{--focusable="false">--}}
+                                    {{--<symbol id="dreamsark-polygon" viewBox="0 0 100 100">--}}
+                                        {{--<g>--}}
+                                            {{--<polygon--}}
+                                                    {{--points="5.6,77.4 0,29 39.1,0 83.8,19.3 89.4,67.7 50.3,96.7"></polygon>--}}
+                                            {{--<polygon fill="rgba(255,255,255,0.35)"--}}
+                                                     {{--transform="scale(0.5), translate(50, 50)"--}}
+                                                     {{--points="5.6,77.4 0,29 39.1,0 83.8,19.3 89.4,67.7 50.3,96.7"></polygon>--}}
+                                            {{--<polygon fill="rgba(255,255,255,0.25)"--}}
+                                                     {{--transform="scale(0.25), translate(145, 145)"--}}
+                                                     {{--points="5.6,77.4 0,29 39.1,0 83.8,19.3 89.4,67.7 50.3,96.7"></polygon>--}}
+                                        {{--</g>--}}
+                                    {{--</symbol>--}}
+                                {{--</svg>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+                        {{--<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.17.0/TweenMax.min.js"></script>--}}
+                        {{--<script src="http://tympanus.net/Tutorials/SVGRipples/js/ripple-config.js"></script>--}}
+                    {{--</form>--}}
+
+                    <div class="divider --mini">or login with</div>
+
+                    <div class="segment">
+                        hello world
                     </div>
-                    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.17.0/TweenMax.min.js"></script>
-                    <script src="http://tympanus.net/Tutorials/SVGRipples/js/ripple-config.js"></script>
-                </form>
 
+                </div>
             </div>
 
         </div>
-    </div>
-
-@endsection
-
-@section('contents')
-
-    <div class="segments">
-        <div class="segment --12-columns">
-            <h2>Columns Demo</h2>
-        </div>
-        <div class="segment --12-columns">
-            <div class="item"></div>
-        </div>
-        <div class="segment --1-columns">
-            <div class="item"></div>
-        </div>
-        <div class="segment --11-columns">
-            <div class="item"></div>
-        </div>
-        <div class="segment --2-columns">
-            <div class="item"></div>
-        </div>
-        <div class="segment --10-columns">
-            <div class="item"></div>
-        </div>
-        <div class="segment --3-columns">
-            <div class="item"></div>
-        </div>
-        <div class="segment --9-columns">
-            <div class="item"></div>
-        </div>
-        <div class="segment --4-columns">
-            <div class="item"></div>
-        </div>
-        <div class="segment --8-columns">
-            <div class="item"></div>
-        </div>
-        <div class="segment --5-columns">
-            <div class="item"></div>
-        </div>
-        <div class="segment --7-columns">
-            <div class="item"></div>
-        </div>
-        <div class="segment --6-columns">
-            <div class="item"></div>
-        </div>
-        <div class="segment --6-columns">
-            <div class="item"></div>
-        </div>
-        <div class="segment --7-columns">
-            <div class="item"></div>
-        </div>
-        <div class="segment --5-columns">
-            <div class="item"></div>
-        </div>
-        <div class="segment --8-columns">
-            <div class="item"></div>
-        </div>
-        <div class="segment --4-columns">
-            <div class="item"></div>
-        </div>
-
-        <div class="segment --9-columns">
-            <div class="item"></div>
-        </div>
-        <div class="segment --3-columns">
-            <div class="item"></div>
-        </div>
-        <div class="segment --10-columns">
-            <div class="item"></div>
-        </div>
-        <div class="segment --2-columns">
-            <div class="item"></div>
-        </div>
-        <div class="segment --11-columns">
-            <div class="item"></div>
-        </div>
-        <div class="segment --1-columns">
-            <div class="item"></div>
-        </div>
-        <div class="segment --12-columns">
-            <div class="item"></div>
-        </div>
-
-    </div>
-
-@endsection
-
-@section('content1')
-
-    <div class="container">
-
-        <div class="segment --3-columns --end">
-
-            <div class="segment__header">
-                Login Form
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-            </div>
-
-            <div class="segment__content">
-
-                <form action="{{ route('login.store') }}" method="POST">
-
-                    <div class="form__field">
-                        <input name="login" type="text" placeholder="username or email">
-                    </div>
-
-                    <div class="form__field">
-                        <input name="password" type="password" placeholder="password">
-                    </div>
-
-                    <div class="form__message --error">
-                        <div class="form__message__header">
-                            Oops... something went wrong...
-                        </div>
-                        <ul class="form__message__items">
-                            <li>The login field is required.</li>
-                        </ul>
-                    </div>
-                    <div class="form__field">
-                        <button id="js-ripple-btn" class="button --ripple">
-
-                            Login
-
-                            <svg class="ripple-obj" id="js-ripple">
-                                <use width="4" height="4" xlink:href="#dreamsark-polygon" class="js-ripple"></use>
-                            </svg>
-
-                        </button>
-                        <div style="height: 0; width: 0; position: absolute; visibility: hidden;" aria-hidden="true">
-                            <svg version="1.1" xmlns="http://www.w3.org/2000/svg"
-                                 xmlns:xlink="http://www.w3.org/1999/xlink"
-                                 focusable="false">
-                                <symbol id="dreamsark-polygon" viewBox="0 0 100 100">
-                                    <g>
-                                        <polygon points="5.6,77.4 0,29 39.1,0 83.8,19.3 89.4,67.7 50.3,96.7"></polygon>
-                                        <polygon fill="rgba(255,255,255,0.35)" transform="scale(0.5), translate(50, 50)"
-                                                 points="5.6,77.4 0,29 39.1,0 83.8,19.3 89.4,67.7 50.3,96.7"></polygon>
-                                        <polygon fill="rgba(255,255,255,0.25)"
-                                                 transform="scale(0.25), translate(145, 145)"
-                                                 points="5.6,77.4 0,29 39.1,0 83.8,19.3 89.4,67.7 50.3,96.7"></polygon>
-                                    </g>
-                                </symbol>
-                            </svg>
-                        </div>
-                    </div>
-                    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.17.0/TweenMax.min.js"></script>
-                    <script src="http://tympanus.net/Tutorials/SVGRipples/js/ripple-config.js"></script>
-                </form>
-
-            </div>
-
-            <div class="divider --mini">or login with</div>
-
-            <div class="segment">
-                hello world
-            </div>
-
-        </div>
-
     </div>
 
 @endsection
 
 @section('scripts')
-    {{--var hello = "hello"--}}
-@endsection
-
-@section('contents')
-
-    <div class="row">
-
-        <section class="medium-8 column">
-            <div class="segment"></div>
-        </section>
-
-        <section class="medium-4 column">
-
-            <div class="segment has-attachment">
-
-                <div class="title modern center">Member Login</div>
-
-
-                <div class="title simple center">or login with</div>
-
-                <div class="social center">
-                    <form action="{{ route('login.social.post') }}" method="post">
-                        {{ csrf_field() }}
-                        <ul>
-                            <li>
-                                <input title="Login with QQ" type="image" src="{{ asset('dreamsark-assets/qq-16x16.png') }}"
-                                       name="login_through"
-                                       value="qq"/>
-                            </li>
-                            <li>
-                                <input title="Login with Weibo" type="image" src="{{ asset('dreamsark-assets/weibo-46x16.png') }}" name="login_through"
-                                       value="weibo"/>
-                            </li>
-                            <li>
-                                <input title="Login with Wechat" type="image" src="{{ asset('dreamsark-assets/wechat.png') }}"
-                                       name="login_through" value="weixin"/>
-                            </li>
-                            {{--set hidden as server will be in China and unable to access FB server on ReturnURL from it.
-                            even user have VPN and able to login FB, but code-behind process to read data from FB is not possible.
-                            so disabling it.
-                            <li>
-                                <input type="image" src="{{ asset('dreamsark-assets/facebook.png') }}"
-                                       name="login_through" value="facebook"/>
-                            </li>
-                            --}}
-                        </ul>
-                    </form>
-
-                </div>
-
-            </div>
-
-            <div class="attached center">
-                <small>
-                    <a class="inverted" href="{{ route('register') }}">
-                        Are you a new user?
-                    </a>
-                </small>
-            </div>
-
-        </section>
-
-    </div>
-
+    <script>
+    </script>
 @endsection
