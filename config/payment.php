a662b6c1361e20342673797a3b7a6645fd0ffc98
Vivekanandan Krishnan <vivek@skysoul.com.au>
2016-06-20 17:42:12 +0800

    merchant_id changed for Testing ENV of unionpay

--- config/payment.php
+++ config/payment.php
@@ -38,7 +38,8 @@ return [
              * Test ENV
              */
             'gateway_url' => 'https://101.231.204.80:5000/gateway/api/frontTransReq.do',
-            'service_id' => '700000000000001',
+            'service_id' => '777290058131411',
+//            'service_id' => '700000000000001', ref id given in PRODUCT page
             'private_key_path' => '/var/www/dreamsark/packages/SkysoulPayment/Payment/src/Implementations/Key/Unionpay/700000000000001_acp.pfx',
             'private_key_password' => '000000',
             'public_key_path' => '/var/www/dreamsark/packages/SkysoulPayment/Payment/src/Implementations/Key/Unionpay/verify_sign_acp.cer',
