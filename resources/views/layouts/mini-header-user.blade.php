883ab0553465061cb497065ab3d4d47b43ea525d
Vivekanandan Krishnan <vivek@skysoul.com.au>
2016-06-28 17:03:40 +0800

    user sub-menu modified

--- resources/views/layouts/mini-header-user.blade.php
+++ resources/views/layouts/mini-header-user.blade.php
@@ -19,17 +19,14 @@
                     <div class="ui transparent menu">
                         <a class="item" href="{{ route('user.profile.index') }}">@lang('navbar.profiles')</a>
                         <a class="item" href="{{ route('user.projects') }}">@lang('navbar.my-project')</a>
-                        <a class="item" href="{{ route('user.settings') }}">@lang('navbar.activity')</a>
-                        {{--<div class="ui dropdown item">
+                        {{--                        <a class="item" href="{{ route('user.settings') }}">@lang('navbar.activity')</a>--}}
+                        <div class="ui dropdown item">
                             @lang('navbar.activity')
                             <div class="menu">
-                                <a href="{{ route('user.activity.backed.list') }}" class="item">@lang('navbar.backed-list')</a>
-                                <a href="{{ route('user.activity.enrolled.list') }}" class="item">@lang('navbar.enrolled-list')</a>
-                                <a href="{{ route('user.activity.earning') }}" class="item">@lang('navbar.earning-list')</a>
-                                <a href="{{ route('user.purchase.index') }}" class="item">@lang('navbar.purchases')</a>
                                 <a class="item" href="{{ route('user.settings') }}">@lang('navbar.setting')</a>
+                                <a href="{{ route('user.purchase.index') }}" class="item">@lang('navbar.purchases')</a>
                             </div>
-                        </div>--}}
+                        </div>
                     </div>
                 </div>
             @endif
