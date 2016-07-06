fc06df0133963c40715bb297332494cf929c72f2
Vivekanandan Krishnan <vivek@skysoul.com.au>
2016-07-06 15:03:50 +0800

    qq login

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -3,10 +3,10 @@
 namespace DreamsArk\Http\Controllers\Home;
 
 use DreamsArk\Http\Controllers\Controller;
-use DreamsArk\Http\Requests;
 use Illuminate\Cookie\CookieJar;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Socialite;
 
 class HomeController extends Controller
 {
@@ -20,6 +20,14 @@ class HomeController extends Controller
     public function index(Request $request)
     {
 
+        /**
+         * to get approved in QQ for oAuth Login event
+         * @todo: they instructed to use home page as callback page for first time to get approved and later can be modified to internal pages
+         */
+        if(strpos($request->headers->get('referer'), 'graph.qq.com')!==false || ($request->has('code') && $request->has('state'))){
+            $socialUser = Socialite::driver('qq')->user();
+            dd($socialUser);
+        }
 //        return $this->dispatch(new GetUniqueCharacters());
 
         /**
