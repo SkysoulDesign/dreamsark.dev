52c3f867a6affb6ae92a88bc5956181c66bb3df7
Vivekanandan Krishnan <vivek@skysoul.com.au>
2016-07-07 10:56:29 +0800

    social media login - wechat url changed to qrconnect

--- packages/SkysoulSocialite/Socialite/src/Implementations/WeixinProvider.php
+++ packages/SkysoulSocialite/Socialite/src/Implementations/WeixinProvider.php
@@ -5,22 +5,49 @@ namespace SkysoulDesign\Socialite\Implementations;
 use Laravel\Socialite\Two\User;
 use SocialiteProviders\Weixin\Provider;
 
-/**
- * Created by PhpStorm.
- * User: Vivek
- * Date: 6/23/16
- * Time: 12:16 PM
- */
 class WeixinProvider extends Provider
 {
+    /**
+     * @var string
+     */
+    private $authorizeUrl = 'https://open.weixin.qq.com/connect/oauth2/authorize';
+    /**
+     * @var string
+     */
+    private $qrConnectUrl = 'https://open.weixin.qq.com/connect/qrconnect';
 
+    /**
+     * @param array $user
+     * @return $this
+     */
     protected function mapUserToObject(array $user)
     {
         \Log::info($user);
+
         return (new User())->setRaw($user)->map([
             'id'     => $user['openid'], 'nickname' => $user['nickname'],
             'avatar' => $user['headimgurl'], 'name' => null, 'email' => null,
         ]);
+    }
+
+    /**
+     * {@inheritdoc}.
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase($this->qrConnectUrl, $state);
+    }
+
+    /**
+     * {@inheritdoc}.
+     */
+    protected function buildAuthUrlFromBase($url, $state)
+    {
+        $session = $this->request->getSession();
+
+        $query = http_build_query($this->getCodeFields($state));
+
+        return $url . '?' . $query . '#wechat_redirect';
     }
 
 }
