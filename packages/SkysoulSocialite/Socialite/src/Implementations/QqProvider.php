b74b2b3314f64ce959e31ad7d8082c2ba98bd18b
Vivekanandan Krishnan <vivek@skysoul.com.au>
2016-07-11 11:57:15 +0800

    social media login -QQ token check redirect to login page if its empty

--- packages/SkysoulSocialite/Socialite/src/Implementations/QqProvider.php
+++ packages/SkysoulSocialite/Socialite/src/Implementations/QqProvider.php
@@ -16,7 +16,7 @@ class QqProvider extends Provider
     protected function getUserByToken($token)
     {
         if (is_null($token)) {
-            \Redirect::route('login')->withErrors('Invalid data received');
+            return \Redirect::route('login')->withErrors('Invalid data received');
         }
         $response = $this->getHttpClient()->get('https://graph.qq.com/oauth2.0/me?' . $token);
         \Log::info($response->getBody()->getContents());
@@ -24,7 +24,7 @@ class QqProvider extends Provider
         $this->customOpenId = json_decode($this->removeCallback($response->getBody()->getContents()), true)['openid']??'';
 
         if ($this->customOpenId == '') {
-            \Redirect::route('login')->withErrors('QQ OpenId not received');
+            return \Redirect::to('login')->withErrors('QQ OpenId not received');
         }
 
         $response = $this->getHttpClient()->get(
