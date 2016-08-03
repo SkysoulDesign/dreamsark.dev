3976aa086866c5ca8e642f103820f575873564d3
cloud <mailto:cloud@skysoul.com.au>
2016-08-03 09:52:59 +0800

    remove name instead to use id

--- resources/assets/typescript/Plugins/Items/itemList.ts
+++ resources/assets/typescript/Plugins/Items/itemList.ts
@@ -1,29 +1,29 @@
-var g_ItemData = {
+export var g_ItemData = {
     "GroupAndItem": [
         {"groupID": "A","ItemData": [
-            {"groupID": A,"name": "ItemAA","id": "A01","imageUrl": "Test.png","probabilityToShow": "5",cost: "15"},
-            {"groupID": A,"name": "ItemAB","id": "A02","imageUrl": "Test.png","probabilityToShow": "10",cost: "10"},
-            {"groupID": A,"name": "ItemAC","id": "A03","imageUrl": "Test.png","probabilityToShow": "10",cost: "10"},
-            {"groupID": A,"name": "ItemAD","id": "A04","imageUrl": "Test.png","probabilityToShow": "10",cost: "10"},
-            {"groupID": A,"name": "ItemAE","id": "A05","imageUrl": "Test.png","probabilityToShow": "10",cost: "10"}
+            {"groupID": "A","id": "A01","imageUrl": "Test.png","probabilityToShow": 5,"cost": "15"},
+            {"groupID": "A","id": "A02","imageUrl": "Test.png","probabilityToShow": 10,"cost": "10"},
+            {"groupID": "A","id": "A03","imageUrl": "Test.png","probabilityToShow": 10,"cost": "10"},
+            {"groupID": "A","id": "A04","imageUrl": "Test.png","probabilityToShow": 10,"cost": "10"},
+            {"groupID": "A","id": "A05","imageUrl": "Test.png","probabilityToShow": 10,"cost": "10"}
         ]},
         {"groupID": "B","ItemData": [
-            {"groupID": B,"name": "ItemBA", "id": "B01", "imageUrl": "Test.png", "probabilityToShow": "5", cost: "25"},
-            {"groupID": B,"name": "ItemBB","id": "B02","imageUrl": "Test.png","probabilityToShow": "10",cost: "20"},
-            {"groupID": B,"name": "ItemBC","id": "B03","imageUrl": "Test.png","probabilityToShow": "10",cost: "20"},
-            {"groupID": B,"name": "ItemBD","id": "B04","imageUrl": "Test.png","probabilityToShow": "10",cost: "20"}
+            {"groupID": "B","id": "B01","imageUrl": "Test.png","probabilityToShow": 5, "cost": 25},
+            {"groupID": "B","id": "B02","imageUrl": "Test.png","probabilityToShow": 10,"cost": 20},
+            {"groupID": "B","id": "B03","imageUrl": "Test.png","probabilityToShow": 10,"cost": 20},
+            {"groupID": "B","id": "B04","imageUrl": "Test.png","probabilityToShow": 10,"cost": 20}
         ]},
         {"groupID": "C","ItemData": [
-            {"groupID": C,"name": "ItemCA","id": "C01","imageUrl": "Test.png","probabilityToShow": "5",cost: "35"},
-            {"groupID": C,"name": "ItemCB","id": "C02","imageUrl": "Test.png","probabilityToShow": "10",cost: "30"},
-            {"groupID": C,"name": "ItemCC","id": "C03","imageUrl": "Test.png","probabilityToShow": "10",cost: "30"}
+            {"groupID": "C","id": "C01","imageUrl": "Test.png","probabilityToShow": 5,"cost": 35},
+            {"groupID": "C","id": "C02","imageUrl": "Test.png","probabilityToShow": 10,"cost": 30},
+            {"groupID": "C","id": "C03","imageUrl": "Test.png","probabilityToShow": 10,"cost": 30}
         ]},
         {"groupID": "D","ItemData": [
-            {"groupID": D,"name": "ItemDA","id": "D01","imageUrl": "Test.png","probabilityToShow": "5",cost: "45"},
-            {"groupID": D,"name": "ItemDB","id": "D02","imageUrl": "Test.png","probabilityToShow": "10",cost: "40"}
+           {"groupID": "D","id": "D01","imageUrl": "Test.png","probabilityToShow":  5,"cost": 45},
+            {"groupID":"D","id": "D02","imageUrl": "Test.png","probabilityToShow":  10,"cost": 40}
         ]},
         {"groupID": "E","ItemData": [
-            {"groupID": E,"name": "ItemEA","id": "E01","imageUrl": "Test.png","probabilityToShow": "10",cost: "50"}
+            {"groupID": "E","id": "E01","imageUrl": "Test.png","probabilityToShow": 10,"cost": 50}
         ]}
     ]
 }
